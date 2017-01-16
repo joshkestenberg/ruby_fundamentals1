813c85cec4ad87f112efdccd4f648862a4c90261
joshkestenberg <joshkestenberg@gmail.com>
2017-01-16 14:30:33 -0500

    exercise2 complete

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#tip calc
+puts 55.0 * 0.15
+
+#string to_i
+puts "two" + 2.to_s
+
+#interpolation
+puts "The result of 45628 multiplied by 7839 is #{45628 * 7839}"
+
+#true
+(10 < 20 && 30 < 20) || !(10 == 11)
